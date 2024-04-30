fb109ceb47ec776d82a244a72e4dceee530b5cc9
Leonardo Cesías Díaz <u202010773@upc.edu.pe>
2024-04-30 13:08:02 -0500

    Improved homeScreen

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -2,48 +2,149 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:smartpark_app/pages/parking_detail.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late List<dynamic> _products;
+  late List<dynamic> _filteredProducts;
+
+  @override
+  void initState() {
+    super.initState();
+    _products = [];
+    _filteredProducts = [];
+    _getParkDetails();
+  }
+
+  Future<void> _getParkDetails() async {
+    var response = await http.get(Uri.https("dummyjson.com", 'products'));
+    var data = jsonDecode(response.body);
+    setState(() {
+      _products = data['products'] as List<dynamic>;
+      _filteredProducts = List.from(_products);
+    });
+  }
+
+  void _filterProducts(String query) {
+    setState(() {
+      _filteredProducts = _products
+          .where((product) =>
+          product['title'].toLowerCase().contains(query.toLowerCase()))
+          .toList();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              showSearch(context: context, delegate: ProductSearch(_products, _filterProducts));
+            },
+          ),
+        ],
+      ),
       body: Center(
-        child: FutureBuilder(
-          future: getParkDetails(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return CircularProgressIndicator();
-            } else if (snapshot.hasError) {
-              return Text('Error: ${snapshot.error}');
-            } else {
-              var products = snapshot.data as List<dynamic>;
-              return ListView.builder(
-                itemCount: products.length,
-                itemBuilder: (context, index) {
-                  var product = products[index];
-                  return CardHome(
-                    title: product['title'],
-                    description: product['description'],
-                    price: product['price'],
-                    rating: product['rating'],
-                    imageUrl: product['images'][0],
-                  );
-                },
-              );
-            }
+        child: ListView.builder(
+          itemCount: _filteredProducts.length,
+          itemBuilder: (context, index) {
+            var product = _filteredProducts[index];
+            return GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => ParkingDetail(id: product['id']),
+                  ),
+                );
+              },
+              child: CardHome(
+                title: product['title'],
+                description: product['description'],
+                price: product['price'],
+                rating: product['rating'],
+                imageUrl: product['images'][0],
+              ),
+            );
           },
         ),
       ),
     );
   }
+}
 
-  Future<List<dynamic>> getParkDetails() async {
-    var response = await http.get(Uri.https("dummyjson.com", 'products'));
-    var data = jsonDecode(response.body);
-    var products = data['products'] as List<dynamic>; // Acceder a la lista de productos
-    return products;
+class ProductSearch extends SearchDelegate<String> {
+  final List<dynamic> products;
+  final Function(String) filterProducts;
+
+  ProductSearch(this.products, this.filterProducts);
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: Icon(Icons.clear),
+        onPressed: () {
+          query = '';
+          filterProducts('');
+        },
+      ),
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: Icon(Icons.arrow_back),
+      onPressed: () {
+        close(context, '');
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return _buildSuggestions();
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    return _buildSuggestions();
+  }
+
+  Widget _buildSuggestions() {
+    final List<dynamic> filteredList = query.isEmpty
+        ? products
+        : products
+        .where((product) =>
+        product['title'].toLowerCase().contains(query.toLowerCase()))
+        .toList();
+
+    return ListView.builder(
+      itemCount: filteredList.length,
+      itemBuilder: (context, index) {
+        final product = filteredList[index];
+        return ListTile(
+          title: Text(product['title']),
+          onTap: () {
+            query = product['title'];
+            filterProducts(query);
+            close(context, query);
+          },
+        );
+      },
+    );
   }
 }
 
